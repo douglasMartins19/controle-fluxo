6d0b7344d468632b78152c2cb7d321292bad7149
douglasMartins19 <douglascm19@gmail.com>
2024-10-08 13:32:02 -0300

    Último commit do repositório

---
+++ src/ExemploFor.java
@@ -0,0 +1,11 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        //contando carneirinhos até Teobaldo dormir
+        
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos ++){
+            System.out.println("Contando carneirinhos... " + carneirinhos);
+        }
+
+        System.out.println("Teobaldo dormiu.");
+    }
+}
---
+++ src/ExemploWhile.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.ThreadLocalRandom;
+public class ExemploWhile {
+    public static void main(String[] args) {
+        double mesada = 50.0;
+        while(mesada>0){
+            double valorDoce = valorAleatorio();
+            if (valorDoce > mesada)
+                valorDoce = mesada;
+
+            System.out.println("Doce de valor: R$" + valorDoce + " adicionada ao carrinho.");
+            mesada = mesada - valorDoce;
+        }
+        System.out.println("Mesada: R$" + mesada);
+        System.out.println("Joaozinho gastou toda a sua mesada em doces.");
+    }
+    private static double valorAleatorio() {
+        return ThreadLocalRandom.current().nextDouble(2, 8);
+    }
+}
